71ff047fc4c1fcb8470892bf05677a51f9a914d8
bon ho <82163197+junhyeonsin@users.noreply.github.com>
2023-03-24 21:54:59 +0900

    고치기

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "components/App";
+import { HashRouter } from "react-router-dom";
 import reportWebVitals from "reportWebVitals";
 import "style.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+<HashRouter base="/">
+  <App />
+</HashRouter>;
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
