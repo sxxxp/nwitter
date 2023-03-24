06ba678209c14665423ef663386de9457fb35df1
bon ho <82163197+junhyeonsin@users.noreply.github.com>
2023-03-24 22:22:41 +0900

    ?

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "components/App";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from "reportWebVitals";
 import "style.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-  <HashRouter basename="/">
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
-  </HashRouter>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
